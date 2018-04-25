f20a53bdf00d28708b92365e68ac5ffa888186e9
Kosuke Arisawa <arisawa@gmail.com>
2018-04-25 22:22:38 +0900

    Fix function and method arguments

--- imgconv/imgconv.go
+++ imgconv/imgconv.go
@@ -13,12 +13,6 @@ import (
 
 // Imgconv is used to store options of CLI.
 type Imgconv struct {
-	// in is input directory.
-	in string
-
-	// out is output directory.
-	out string
-
 	// from is image format before conversion
 	from string
 
@@ -45,21 +39,7 @@ func SupportedFormats() string {
 }
 
 // NewImgconv allocates a new Imgconv struct and detect error.
-func NewImgconv(in, out, from, to string, verbose bool) (*Imgconv, error) {
-	stat, err := os.Stat(in)
-	if err != nil {
-		return &Imgconv{}, err
-	}
-	if !stat.IsDir() {
-		return &Imgconv{}, fmt.Errorf("in:%s is not directory", in)
-	}
-	stat, err = os.Stat(out)
-	if err != nil {
-		return &Imgconv{}, err
-	}
-	if !stat.IsDir() {
-		return &Imgconv{}, fmt.Errorf("out:%s is not directory", out)
-	}
+func NewImgconv(from, to string, verbose bool) (*Imgconv, error) {
 	if _, ok := supportedFormats[from]; !ok {
 		return &Imgconv{}, fmt.Errorf("from:%s is not supported", from)
 	}
@@ -69,25 +49,34 @@ func NewImgconv(in, out, from, to string, verbose bool) (*Imgconv, error) {
 	if from == to {
 		return &Imgconv{}, fmt.Errorf("same formats are specified")
 	}
-	return &Imgconv{in, out, from, to, verbose}, nil
+	return &Imgconv{from, to, verbose}, nil
 }
 
 // Do executes image conversion for target files.
-func (c *Imgconv) ConvertRecursively() error {
-	err := filepath.Walk(c.in, func(path string, info os.FileInfo, err error) error {
+func (c *Imgconv) ConvertRecursively(in, out string) error {
+	for _, dir := range []string{in, out} {
+		stat, err := os.Stat(dir)
+		if err != nil {
+			return err
+		}
+		if !stat.IsDir() {
+			return fmt.Errorf("%s is not directory", dir)
+		}
+	}
+
+	err := filepath.Walk(in, func(src string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if info.IsDir() {
 			return nil
 		}
-		if filepath.Ext(path) != fmt.Sprintf(".%s", c.from) {
-			c.vLog("format is not match %s: %s", path, c.from)
+		if filepath.Ext(src) != fmt.Sprintf(".%s", c.from) {
+			c.vLog("format is not match %s: %s", src, c.from)
 			return nil
 		}
 
-		err = c.Convert(path)
-		if err != nil {
+		if err = c.Convert(src, out); err != nil {
 			return err
 		}
 
@@ -101,18 +90,9 @@ func (c *Imgconv) ConvertRecursively() error {
 	return nil
 }
 
-// convert executes image conversion a source file.
-func (c *Imgconv) Convert(src string) error {
-	destPath := strings.Split(src, "/")
-	basename := filepath.Base(src)
-	destPath[0] = c.out
-	destPath[len(destPath)-1] = strings.Replace(basename, c.from, c.to, 1) // fix filename
-	dest := filepath.Join(destPath...)
-
-	destDir := filepath.Dir(dest)
-	if _, err := os.Stat(destDir); err != nil {
-		os.MkdirAll(destDir, os.ModePerm)
-	}
+// Convert executes image conversion a source file to the dest file.
+func (c *Imgconv) Convert(src, out string) error {
+	dest := c.buildDestPath(src, out)
 
 	file, err := os.Open(src)
 	if err != nil {
@@ -124,25 +104,40 @@ func (c *Imgconv) Convert(src string) error {
 		return err
 	}
 
-	out, err := os.Create(dest)
+	w, err := os.Create(dest)
 	if err != nil {
 		return err
 	}
-	defer out.Close()
+	defer w.Close()
 
 	switch c.to {
 	case "png":
-		err = png.Encode(out, img)
+		err = png.Encode(w, img)
 	case "jpg":
-		err = jpeg.Encode(out, img, &jpeg.Options{Quality: 100})
+		err = jpeg.Encode(w, img, &jpeg.Options{Quality: 100})
 	case "gif":
-		err = gif.Encode(out, img, &gif.Options{NumColors: 256})
+		err = gif.Encode(w, img, &gif.Options{NumColors: 256})
 	}
 	if err != nil {
 		return err
 	}
 	c.vLog("convert %s to %s", src, dest)
 	return nil
+}
+
+// buildDestPath creates the destination file path.
+func (c *Imgconv) buildDestPath(src, out string) string {
+	destPath := strings.Split(src, "/")
+	basename := filepath.Base(src)
+	destPath[0] = out
+	destPath[len(destPath)-1] = strings.TrimSuffix(basename, filepath.Ext(basename)) + "." + c.to
+	dest := filepath.Join(destPath...)
+
+	destDir := filepath.Dir(dest)
+	if _, err := os.Stat(destDir); err != nil {
+		os.MkdirAll(destDir, os.ModePerm)
+	}
+	return dest
 }
 
 // vLog prints log when verbose is set.
--- main.go
+++ main.go
@@ -34,11 +34,11 @@ func main() {
 		log.Fatalf("%s: invalid argument", os.Args[0])
 	}
 
-	c, err := imgconv.NewImgconv(*in, *out, *from, *to, *verbose)
+	c, err := imgconv.NewImgconv(*from, *to, *verbose)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := c.ConvertRecursively(); err != nil {
+	if err := c.ConvertRecursively(*in, *out); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s: conversion finished\n", os.Args[0])
